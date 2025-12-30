1553239b3cbbf8fc9a6f7cd53d0bdf1aa96cc90a
greatdevan13-byte <greatdevan13@gmail.com>
2025-12-30 17:32:10 +0530

    Add initial implementation of fruit sum calculation and random number generation

---
+++ cw1.py
@@ -0,0 +1,16 @@
+import random
+apple=15.5
+orange=20
+grape=10.25
+sum_fruits=apple+orange+grape
+print(sum_fruits)
+sum_fruits=int(sum_fruits)
+print(sum_fruits)
+sum_fruits=str(sum_fruits)
+print("I convert the value to string: ",sum_fruits)
+sum_fruits=int(sum_fruits)
+
+
+a=random.randrange(5,10)
+
+print(a+sum_fruits)
