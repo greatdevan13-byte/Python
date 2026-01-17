6ff1b5291bb035a3c295e195a35edc5e6825f875
greatdevan13-byte <greatdevan13@gmail.com>
2026-01-17 11:39:38 +0530

    Refactor input handling for book title and publication year with validation

--- regexcw12.py
+++ regexcw12.py
@@ -1,2 +1,24 @@
-input_title=input("Enter a book title")
-input_year=int(input("Publication year"))
+import re
+
+try:
+    title = input("Enter book title: ")
+
+ 
+    if not re.fullmatch("[a-zA-Z ]+", title):
+        raise ValueError("Invalid title")
+
+    year = input("Enter publication year: ")
+
+  
+    if not re.fullmatch("(19|20)[0-9]{2}", year):
+        raise ValueError("Invalid publication year")
+
+    print("\nBook details accepted")
+    print("Title:", title)
+    print("Year:", year)
+
+except:
+    print("\nError: Invalid input")
+
+finally:
+    print("\nProgram ended")
